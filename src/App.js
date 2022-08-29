8dff06d63133682777a5bc9d16d867c57d948556
Jung Choi <56977454+choijung121@users.noreply.github.com>
2022-08-29 11:56:27 -0700

    fixed API format

--- src/App.js
+++ src/App.js
@@ -1,65 +1,41 @@
-import React,{useState, useEffect } from 'react';
-
+import React, { useEffect, useState } from "react";
+import Weather from './component/weather';
 import './App.css';
+// import 'semantic-ui-css/semantic.min.css'
 
 function App() {
-  const [apiData, setApiData] = useState({});
-  const [getState, setGetState] = useState('');
-  const [state, setState] = useState('');
-
-  const API_KEY = process.env.REACT_APP_API_KEY; 
-  const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=q=${state}&appid=${API_KEY}`;
+  const [lat, setLat] = useState([]); /* setting latitude */
+  const [long, setLong] = useState([]); /* setting longitude */
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch(API_URL)
-      .then((res) => res.json())
-      .then((data) => setApiData(data));
-  }, [API_URL]);
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function(position){
+        setLat(position.coords.latitude); 
+        setLong(position.coords.longitude); 
+      }); 
+  
+      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+      /* https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key} */
+        .then(response => response.json())
+        .then(result => {
+          setData(result)
+          console.log(result); 
+        })
+      console.log("Latitude: ", lat); 
+      console.log("Longitude: ", long); 
+    }
+    fetchData(); 
+  }, [lat, long]); 
 
-  function inputHandler(event) {
-    setGetState(event.target.value); 
-  }
-
-  function submitHandler(){
-    setState(getState); 
-  }
 
   return (
     <div className="App">
-        <div className="project-title-div">
-            <h1 className="project-title">Weather Search</h1>   
-        </div>
-        <div className="nav-area">
-            <input 
-              type="text" 
-              className="text-box" 
-              placeholder="Search City" 
-              onChange={inputHandler}
-              value={getState}>
-            </input>
-            <button className="btn btn-primary" onClick={submitHandler}>
-                Search
-            </button>
-        </div>
-        
-        <div className="card">
-            <div class="container">
-              <div class='row'>
-                <div class='weather-icon col-sm-6'>
-                  <img
-                    src={`http://openweathermap.org/img/w/${apiData.weather[0].icon}.png`}
-                    alt="weather status icon"
-                    className="weather-icon"
-                  />
-                </div>
-              </div>
-              <div class='row'>
-                <div class='weather-info col-sm-6'>
-                  <strong>{apiData.name}</strong>
-                </div>
-              </div>
-            </div>
-        </div>
+      {(typeof data.main != 'undefined') ? (
+        <Weather weatherData={data}/>
+      ): (
+        <div></div>
+      )}
     </div>
   );
 }
