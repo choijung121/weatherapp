48ba82a2ef0cf552e5e83b86dffa147d91067bbe
Jung Choi <56977454+choijung121@users.noreply.github.com>
2022-08-29 23:06:20 -0700

    search working, had to remove navigator.geolocation

--- src/App.js
+++ src/App.js
@@ -8,16 +8,17 @@ function App() {
   const [lat, setLat] = useState([]); /* setting latitude */
   const [long, setLong] = useState([]); /* setting longitude */
   const [data, setData] = useState([]);
+  const [getInputData, setInputData] = useState(""); 
+  const [cityName, setSubmitData] = useState("")
 
   useEffect(() => {
     const fetchData = async () => {
-      navigator.geolocation.getCurrentPosition(function(position){
-        setLat(position.coords.latitude); 
-        setLong(position.coords.longitude); 
-      }); 
+      // navigator.geolocation.getCurrentPosition(function(position){
+      //   setLat(position.coords.latitude); 
+      //   setLong(position.coords.longitude); 
+      // }); 
   
-      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&APPID=${process.env.REACT_APP_API_KEY}`)
-      /* https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key} */
+      await fetch(`${process.env.REACT_APP_API_URL}/weather/?q=${cityName}&lat=${lat}&lon=${long}&APPID=${process.env.REACT_APP_API_KEY}`)
         .then(response => response.json())
         .then(result => {
           setData(result)
@@ -25,9 +26,18 @@ function App() {
       })
       console.log("Latitude: ", lat); 
       console.log("Longitude: ", long); 
+      console.log("City Name:", cityName); 
     }
     fetchData(); 
-  }, [lat, long]); 
+  }, [lat, long, cityName]); 
+
+  const inputHandler = (event) => {
+    setInputData(event.target.value); 
+  }
+
+  const submitHandler = () => {
+    setSubmitData(getInputData); 
+  }
 
 
   return (
@@ -35,6 +45,19 @@ function App() {
       <p>Day: {moment().format('dddd')}</p>
       <p>Date: {moment().format('LL')}</p>
       <p>Time: {moment().format("h:mm a")}</p>
+      <div className="input">
+        <input 
+          type="text"
+          id="location-name"
+          class="form-control"
+          placeholder="Enter City Name"
+          onChange={inputHandler}
+          value={getInputData}>
+        </input>
+      </div>
+      <button className="btn btn-primary mt-2" onClick={submitHandler}>
+        Search
+      </button>
       {(typeof data.main != 'undefined') ? (
         <Weather weatherData={data}/>
       ): (
--- src/component/weather.js
+++ src/component/weather.js
@@ -1,26 +1,15 @@
 import React from 'react';
 import Card from "react-bootstrap/Card";
-import moment from "moment"; 
 
 const KelvinToFahrenheit = (k) => {
     let temperature = ((k - 273.15) * (9/5) + 32).toFixed(2);
     return Math.round(temperature);
 }
 
-
-const weatherIcon = this.state.weatherData.weather.map(w => {
-    return <img src={`${process.env.REACT_APP_ICON_URL}/${w.icon}.png`}></img>
-}); 
-    // const renderWeatherIcon = this.state.data.weather.map(item => {
-    //     return <img src={`http://openweathermap.org/img/w/${item.icon}.png`} />;
-    //   });
-    //   return <div>{renderWeatherIcon}</div>;
-
-
 const CardExampleCard = ({weatherData}) => (
   <Card>
     <Card.Header className="header">{weatherData.name}</Card.Header>
-    <div>{weatherIcon}</div>
+    <img src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}></img>
     <p>{KelvinToFahrenheit(weatherData.main.temp)}&deg; F</p>
     <p>{weatherData.weather[0].main}</p>
     <p>H: {KelvinToFahrenheit(weatherData.main.temp_max)}&deg; F</p>
